13140e5eb411ed583f78068d48872e30c60adf48
Rrezon Krasniqi <rrezonkrasniqi32@gmail.com>
2024-11-10 17:26:47 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./Components/Main";
 
+
+const routes = [
+  {
+    path: "/",
+    element: <Main/>
+  },
+
+];
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div className="App">
+
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='bg-slate-400'>Main</div>
+  )
+}
+
+export default Main
